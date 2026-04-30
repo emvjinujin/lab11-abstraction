adf19b08325aec2afdd737ae2e2a8d20be1fbb6a
b24fm1032 <emvjn44@gmail.com>
2026-04-30 08:33:30 -0500

    Implement use method for Scroll class

--- assignments/skills/Scroll.java
+++ assignments/skills/Scroll.java
@@ -1,6 +1,10 @@
 public class Scroll implements Usable {
 
-    // TODO: @Override use(Character user) → void
-    // - user.mp += 30 (MP сэргээнэ)
-
+    @Override
+    public void use(Character user) {
+    
+        user.mp += 30;
+        
+        System.out.println(user.getName() + " reads a magical Scroll and recovered 30 MP!");
+    }
 }
