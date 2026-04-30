9144de940c9d14dc7ca97e2756ea237ed952af52
b24fm1032 <emvjn44@gmail.com>
2026-04-30 08:34:38 -0500

    Implement Heal constructor and cast method

--- assignments/skills/Heal.java
+++ assignments/skills/Heal.java
@@ -1,11 +1,17 @@
 public class Heal extends Skill {
 
-    // TODO: Constructor бичнэ үү
-    // public Heal()
-    // - super("Heal", 20) дуудна
+    public Heal() {
+        super("Heal", 20);
+    }
 
-    // TODO: @Override cast(Character caster, Character target) → void
-    // - caster.heal(30) (target-ийг үл тоомсорлоно)
-    // - caster.mp -= 20
-
+    
+    @Override
+    public void cast(Character caster, Character target) {
+    
+        caster.heal(30);
+    
+        caster.mp -= 20;
+        
+        System.out.println(caster.getName() + " casted Heal and recovered HP!");
+    }
 }
